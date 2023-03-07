c887e1767eaccbe5f8455ec30c25774980fdb311
WojciechPtas <wojciech.ptas@gmail.com>
2023-03-07 13:27:40 +0100

    refactoring of unittesst

--- distance_tests.py
+++ distance_tests.py
@@ -3,6 +3,14 @@ import pandas as pd
 import numpy as np
 import unittest
 
+def initializeRecord(row:pd.Series):
+        a = maping.Point(row['xa'],row['ya'])
+        b = maping.Point(row['xb'],row['yb'])
+        res = maping.Point(row['xs'],row['ys'])
+        distance_a = row['a']
+        distance_b = row['b']
+        return (a,b,res,distance_a,distance_b)
+    
 class TestofTests(unittest.TestCase):
     """test"""
     def test_1(self):
@@ -18,24 +26,26 @@ class DistanceTests(unittest.TestCase):
     to run:
     python -m unittest distance_tests.DistanceTests
     """
-    def setUp(self):
-        pass
-        self.df:pd.DataFrame = pd.read_csv("./tests/regular.csv")
-    def test_SyntheticData(self):
+    def initializeDF(self, name :str):
+        self.df = pd.read_csv("./tests/" + name + ".csv")
+        assert self.df.size > 0
+    def runTest(self):
         for index,row in self.df.iterrows():
-            a = maping.Point(row['xa'],row['ya'])
-            b = maping.Point(row['xb'],row['yb'])
-            res = maping.Point(row['xs'],row['ys'])
-            
-            distance_a = row['a']
-            distance_b = row['b']
-            
             print(row)
             
+            a,b,res,distance_a,distance_b = initializeRecord(row)
+                        
             with self.subTest(msg=f"{index}th Test: "):
                 result: maping.Point = maping.calculate_position(a,b,res,distance_a, distance_b)
+                
                 print(f"Expected: x = {res.x},y = {res.y},\nResult: x = {result.x}, y = {result.y}")
                 
                 self.assertAlmostEqual(result.x,res.x,)
                 self.assertAlmostEqual(result.y,res.y)
-
+    def test_SyntheticData(self):
+        self.initializeDF("regular")
+        self.runTest()
+    def test_RealData(self):
+        self.initializeDF("real1")
+        self.runTest()
+                
