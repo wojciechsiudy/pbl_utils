2bdcbfb87a72080890b3350a610e353d385741c6
Wojciech Siudy <wojcsiu541@student.polsl.pl>
2023-03-07 17:57:51 +0100

    ranging: Introduce reset
    When the connection fails some errors are thrown
    and caught in order to restart connection
    
    This feature may lead to freeze, so watchdog
    should be reintroduced in near future
    
    Signed-off-by: Wojciech Siudy <wojcsiu541@student.polsl.pl>

--- ranging.py
+++ ranging.py
@@ -1,132 +1,94 @@
-from multiprocessing import Queue
-from threading import Thread
-from serial import Serial, SerialException
-from time import sleep
+import json
+import BLE_GATT
 
-BAUDRATE = 115200
-MAX_FAILS = 1
-FRAMES_AMOUNT = 10
-THREAD_TIMEOUT = 0.6
+class UwbDataError(Exception):
+    pass
 
-DEBUG = True
+class ConnectionError(Exception):
+    pass
 
-
-class UwbSerialConnection:
-    def __init__(self, serial_path="/dev/UWBt") -> None:
-        self.serial_path = serial_path
-        self.serial = Serial()
-        self.connected = False
-        self.distances_queue = Queue()
-        self.last_value = 0
-        self.fails = 0
-        self.address_queue = Queue()
-        self.last_address = "none"
-        self.alive_ping = Queue()
-        self._set_threads()
-
-    def begin(self):
-        self._connect()
-        self.process.start()
-        self.watchdog.start()
-
-    def _set_threads(self):
-        self.watchdog = Thread(target=self._watchdog_process, args=(self.alive_ping,))
-        self.process = Thread(target=self._thread_process,
-                              args=(self.distances_queue, self.address_queue, self.alive_ping,))
-
-    def end(self):
-        self._disconnect()
-        self._set_threads()
-
-    def restart(self):
-        self.end()
-        self.begin()
-        if DEBUG is True:
-            print("RESTARTED")
-
-    def _watchdog_process(self, ping_queue):
-        while True:
-            if ping_queue.qsize() > MAX_FAILS:
-                self.restart()
-                return
-            sleep(THREAD_TIMEOUT)
-
-    def _thread_process(self, queue, address, ping_queue):
-        current_address = self.last_address
-        while True:
-            if DEBUG is True:
-                print("THREAD ", self.serial_path, " ALIVE, address set: ", current_address)
-            if ping_queue.qsize() > 0:
-                ping_queue.get("X")
-            if self.address_queue.qsize() > 0:
-                current_address = address.get()
-                if DEBUG:
-                    print("ADDRESS SET: ", current_address)
-            message = str(current_address) + str(FRAMES_AMOUNT)
-            if self.serial.isOpen() and current_address != "none":
-                try:
-                    if self.serial_path == "/dev/UWBl":
-                        sleep(0.007)
-                        if DEBUG:
-                            print("SMALL SLEEP")
-                    elif self.serial_path == "/dev/UWBr":
-                        sleep(0.053)
-                        if DEBUG:
-                            print("DEEP SLEEP")
-                    #begin communication
-                    self.serial.write(bytes(message, "ASCII"))
-                    for i in range(0, FRAMES_AMOUNT):
-                        line = str(self.serial.readline(), encoding="ASCII")
-                        try:
-                            data = line.split('|')
-                            distance = data[1]
-                            if DEBUG:
-                                print("I: ", i, "DISTANCE: ", distance)
-                            queue.put(distance)
-                        except (ValueError, IndexError):  # in case of reciver failure
-                            print("error")
-                except(TypeError, SerialException, OSError):
-                    if DEBUG:
-                        print("SERIAL FAILURE")
-                    return
-            else:
-                self.reconnect()
-            sleep(THREAD_TIMEOUT)
-
-    def _connect(self):
-        self.serial = Serial(self.serial_path, BAUDRATE)
-        self.connected = True
-
-    def _disconnect(self):
-        self.serial.close()
-        self.connected = False
-
-    def reconnect(self):
-        self.fails = 0
-        self._disconnect()
-        self._connect()
-
-    def _test_connection(self):
-        if self.serial.isOpen():
-            self.connected = True
-        else:
-            self.connected = False
-
-    # this method creates interface between ROS2 and serial connection
-    def get_distance(self):
-        if self.distances_queue.qsize() > 0:
-            self.last_value = self.distances_queue.get()
-        return self.last_value
-
-    def set_address(self, _address):
-        if _address != self.last_address:
-            self.last_address = _address
-            self.address_queue.put(self.last_address)
-            if DEBUG:
-                print("SET_ADDRESSS_CALLED: ", _address)
-        self.alive_ping.put("X")
-
+class UwbData:
+    def __init__(self, data: str):
+        data_array = data.split("|")
+        if len(data_array) < 2:
+            raise UwbDataError
+        self.tag_address = data_array[0][:5]
+        self.distance = float(data_array[1])
+        self.power = float(data_array[2])
 
 class UwbBluetoothConnection:
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.debug_level = 0 # 0 is silent, 3 speaks a lot
+        self.read_settings_from_json()
+        self.device = BLE_GATT.Central(self.uwb_mac_adress)
+
+    def read_settings_from_json(self):
+        try:
+            file = open("settings.json")
+        except FileNotFoundError:
+            self.debug("Settings file not found! Aborting setup.", 1)
+            return
+        settings = json.load(file)
+        self.uwb_mac_adress = settings["DEVICE_ADDRESS"]
+        self.service_uuid = settings["SERVICE_UUID"]
+        self.read_characteristic = settings["READ_CHARACTERISTIC_UUID"]
+        self.write_characteristic = settings["WRITE_CHARACTERISTIC_UUID"]
+
+    def debug(self, message: str, level=3):
+        if self.debug_level >= level:
+            print(message)
+
+    def connect(self):
+        self.device.connect()
+
+    def ask_for_distance(self, address: str, amount=1):
+        message = address + str(amount)
+        self.debug("Sending " + str(amount) + " packet(s) to " + address, 3)
+        try:
+            self.device.char_write(self.write_characteristic, bytes(message, 'utf-8'))
+        except KeyError:
+            self.debug("Write error. Probably UUID not found", 1)
+        except:
+            self.debug("Unknown BLE error", 2)
+            raise ConnectionError
+
+    def read_anwser(self) -> UwbData:
+        try:
+            raw_anwser = self.device.char_read(self.read_characteristic)
+        except:
+            self.debug("Unknown BLE error", 2)
+            raise ConnectionError
+        self.debug("Raw data is: " + str(raw_anwser), 3)
+        anwser = ""
+        for byte in raw_anwser:
+            anwser += chr(byte)
+        return UwbData(anwser)
+    
+    def tmp_get_distance(self, address: str) -> float:
+        try:
+            self.ask_for_distance(address)
+            uwb_data = self.read_anwser()
+        except (ConnectionError, UwbDataError):
+            self.debug("Connection failed", 2)
+            self.restart()
+            return 0
+        return uwb_data.distance
+
+
+    def disconnect(self):
+        self.device.disconnect()
+
+    def restart(self):
+        self.disconnect()
+        self.connect()
+
+
+# sample to be deleted soon
+connection = UwbBluetoothConnection()
+connection.connect()
+connection.debug_level = 3
+
+
+for i in range(30):
+    print(connection.tmp_get_distance("AA:04"))
+    print(connection.tmp_get_distance("21:37"))
