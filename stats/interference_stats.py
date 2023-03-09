d078cbf8b704e979be9fe86960d6a7d7f974f7af
Jan Kocurek <jas.kocurek2@wp.pl>
2023-03-09 14:25:12 +0100

    serial opening

--- stats/interference_stats.py
+++ stats/interference_stats.py
@@ -3,17 +3,19 @@ from threading import Thread
 
 FRAMES_AMOUNT = 100
 
-SERIAL_PORT_1 = "COM5"
-SERIAL_PORT_2 = "COM6"
+BAUDRATE = 115200
+
+SERIAL_PORT_1 = "COM12"
+SERIAL_PORT_2 = "COM8"
 
 ANCHOR_ADDRESS_1 = "AA:BB"
 ANCHOR_ADDRESS_2 = "CC:DD"
 
-def serial_read_write(message, filename):
-    Serial.serial.write(bytes(message, "ASCII"))
+def serial_read_write(message, filename, serial):
+    serial.write(bytes(message, "ASCII"))
     lines = ""
     for _ in range(FRAMES_AMOUNT):
-        lines += str(Serial.serial.readline(), encoding="ASCII")
+        lines += str(serial.readline(), encoding="ASCII")
 
     f = open(filename, "w+")
     f.write(lines)
@@ -23,11 +25,14 @@ iterations = int(input("Input iterations number\n"))
 file_name_prefix = input("Input filename prefix\n")
 
 for i in range(iterations):
-    message_1 = str(ANCHOR_ADDRESS_1) + str(FRAMES_AMOUNT)
-    message_2 = str(ANCHOR_ADDRESS_2) + str(FRAMES_AMOUNT)
+    message_1 = ANCHOR_ADDRESS_1 + str(FRAMES_AMOUNT)
+    message_2 = ANCHOR_ADDRESS_2 + str(FRAMES_AMOUNT)
+
+    serial_1 = Serial(SERIAL_PORT_1, BAUDRATE)
+    serial_2 = Serial(SERIAL_PORT_2, BAUDRATE)
     
-    t1 = Thread(target=serial_read_write, args=(message_1, file_name_prefix + "1"))
-    t2 = Thread(target=serial_read_write, args=(message_2, file_name_prefix + "2"))
+    t1 = Thread(target=serial_read_write, args=(message_1, file_name_prefix + "1", serial_1))
+    t2 = Thread(target=serial_read_write, args=(message_2, file_name_prefix + "2", serial_2))
 
     t1.start()
     t2.start()
