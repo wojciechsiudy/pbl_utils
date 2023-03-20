67de9b856ea83332000c6984df8963bd43c7a8e6
sq7rai <artujan710@student.polsl.pl>
2023-03-20 13:25:54 +0100

    Update mapping.py
    Deleted big comment and simplified distance_a and distance_b vars.

--- mapping.py
+++ mapping.py
@@ -51,27 +51,25 @@ def calculate_position(anchor_A: Point, anchor_B: Point, ctrl_anchor: Point,
     
     d = math.sqrt((anch_xy_B[0] - anch_xy_A[0]) ** 2 + (anch_xy_B[1] - anch_xy_A[1]) ** 2)
     
-    ra = distance_a
-    rb = distance_b
     # non intersecting
-    if d > ra + rb:
-        while (ra + rb < d and scale_offset_factor < MAX_UWB_OFFSET_FACTOR):
+    if d > distance_a + distance_b:
+        while (distance_a + distance_b < d and scale_offset_factor < MAX_UWB_OFFSET_FACTOR):
             scale_offset_factor += 1.005
             if power_a>power_b:
-                rb *= scale_offset_factor
+                distance_b *= scale_offset_factor
             elif power_a<power_b:
-                ra *= scale_offset_factor
-    if d > ra + rb:
+                distance_a *= scale_offset_factor
+    if d > distance_a + distance_b:
         return Point(0, 0)
     # One circle within other
-    if d < abs(ra - rb):
+    if d < abs(distance_a - distance_b):
         return Point(0, 0)
     # coincident circles
-    if d == 0 and ra == rb:
+    if d == 0 and distance_a == distance_b:
         return Point(0, 0)
     else:
-        a = (ra ** 2 - rb ** 2 + d ** 2) / (2 * d)
-        h = math.sqrt(ra ** 2 - a ** 2)
+        a = (distance_a ** 2 - distance_b ** 2 + d ** 2) / (2 * d)
+        h = math.sqrt(distance_a ** 2 - a ** 2)
         x2 = anch_xy_A[0] + a * (anch_xy_B[0] - anch_xy_A[0]) / d
         y2 = anch_xy_A[1] + a * (anch_xy_B[1] - anch_xy_A[1]) / d
         x3 = x2 + h * (anch_xy_B[1] - anch_xy_A[1]) / d
@@ -91,75 +89,6 @@ def calculate_position(anchor_A: Point, anchor_B: Point, ctrl_anchor: Point,
         else:
             position = transformer2.transform(x4, y4)
             return Point(position[0], position[1])
-
-
-'''def calculate_position(anchor_A:Point, anchor_B:Point,\
-    ctrl_anchor:Point, distance_a:float, distance_b:float):  # anchor_A ze współrzędnymi "(0,0)", distance_a/b od anchorów
-
-    try:
-        # c = anchor_A.get_distance_to(anchor_B) #odległośc między anchorami
-        c = get_distance(anchor_A,anchor_B)
-        scale_offset_factor = 1.0
-        while (distance_a + distance_b < c and scale_offset_factor < MAX_UWB_OFFSET_FACTOR):
-            scale_offset_factor += 1.005
-            distance_a *= scale_offset_factor
-            distance_b *= scale_offset_factor
-        # print("C:", c)
-        cos_a = (pow(distance_b, 2) + pow(c, 2) - pow(distance_a, 2)) / abs(2 * distance_b * c)
-        #cos_a = (pow(distance_a, 2) + pow(distance_b, 2) - pow(c, 2)) / (2 * (distance_a * distance_b))
-        print("COS_A:",cos_a)
-        #tempy = float(((distance_b * cos_a) / 1852) / 60)
-        tempy = float((distance_b * cos_a) / 111139)
-        #tempx = float(((distance_b * math.sqrt(1 - cos_a * cos_a)) / 1852) / 60)
-        tempx = float((distance_b * math.sqrt(1 - cos_a * cos_a)) / 111139)
-        #print("tempx:", tempx, "tempy:", tempy)
-        if anchor_A.x > ctrl_anchor.x:
-            x = anchor_A.x - tempx
-        else:
-            x = anchor_A.x + tempx
-        if anchor_A.y > ctrl_anchor.y:
-            y = anchor_A.y - tempy
-        else:
-            y = anchor_A.y + tempy
-    except (ZeroDivisionError, ValueError, AttributeError):
-        return Point(0.0, 0.0)
-    return Point(x, y)
-
-def calc_circle(anchor_A, anchor_B, ctrl_anchor, distance_a, distance_b):
-    # circle 1: (x0, y0), radius r0
-    # circle 2: (x1, y1), radius r1
-
-    d = math.sqrt((anchor_B.x - anchor_A.x) ** 2 + (anchor_B.y - anchor_A.y) ** 2)
-    ra = distance_a/111139
-    rb = distance_b/111139
-    # non intersecting
-    if d > ra + rb:
-        return Point(0, 0)
-    # One circle within other
-    if d < abs(ra - rb):
-        return Point(0, 0)
-    # coincident circles
-    if d == 0 and ra == rb:
-        return Point(0, 0)
-    else:
-        a = (ra ** 2 - rb ** 2 + d ** 2) / (2 * d)
-        h = math.sqrt(ra ** 2 - a ** 2)
-        x2 = anchor_A.x + a * (anchor_B.x - anchor_A.x) / d
-        y2 = anchor_A.y + a * (anchor_B.y - anchor_A.y) / d
-        x3 = x2 + h * (anchor_B.y - anchor_A.y) / d
-        y3 = y2 - h * (anchor_B.x - anchor_A.x) / d
-
-        x4 = x2 - h * (anchor_B.y - anchor_A.y) / d
-        y4 = y2 + h * (anchor_B.x - anchor_A.x) / d
-
-        #return (x3, y3, x4, y4)
-        pa = (x3, y3)
-        pb = (x4, y4)
-        pg = (ctrl_anchor.x, ctrl_anchor.y)
-        if geodesic(pa, pg).m < geodesic(pb, pg).m:
-            return Point(x3, y3)
-        else:
-            return Point(x4, y4)'''
 
 def gps_data_to_point(data):
     """
