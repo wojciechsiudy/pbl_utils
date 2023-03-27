3c230d1c7a2eedd427f3f675230d61ce7aed9834
Wojciech Siudy <wojcsiu541@student.polsl.pl>
2023-03-27 13:38:28 +0200

    inercing: Fix serial read and sync
    Signed-off-by: Wojciech Siudy <wojcsiu541@student.polsl.pl>

--- inercing.py
+++ inercing.py
@@ -1,12 +1,15 @@
 """
-authors: Maciej Bolesta, Wojciech Siudy
+authors: Maciej Bolesta, Wojciech Ptaś, Wojciech Siudy
 """
+
 from sys import stdout
 from serial import Serial
 from multiprocessing import Queue, Process
-#from threading import Thread
 
 from .misc import StampedData
+
+class AhrsReadError(Exception):
+    pass
 
 class InercialPoint:
     def __init__(self, x = 0.0, y = 0.0, z = 0.0):
@@ -15,64 +18,75 @@ class InercialPoint:
         self.z = z
 
 class AhrsData(StampedData):
-    def __init__(self):
-        self.accel = InercialPoint()
-        self.gyro = InercialPoint()
-        self.mag = InercialPoint()
     def __init__(self,
-                 accel: InercialPoint,
-                 gyro: InercialPoint,
-                 mag: InercialPoint):
+                 accel = InercialPoint(),
+                 gyro = InercialPoint(),
+                 mag = InercialPoint()):
         self.accel = accel
         self.gyro = gyro
         self.mag = mag
 
+    def __repr__(self) -> str:
+        value = ""
+        value = value + str(self.accel.x) + ", "
+        value = value + str(self.accel.y) + ", "
+        value = value + str(self.accel.z) + ", "
+        value = value + str(self.gyro.x) + ", "
+        value = value + str(self.gyro.y) + ", "
+        value = value + str(self.gyro.z) + ", "
+        value = value + str(self.mag.x) + ", "
+        value = value + str(self.mag.y) + ", "
+        value = value + str(self.mag.z)
+        return value
+
 class AhrsConnection:
     def __init__(self, serial_path="/dev/AHRS"):
         self.ahrs_serial = Serial(serial_path, 115200)
-        self.measures_queue = Queue()
-        self.last_value = AhrsData() # todo: initialisation
+        self.measures_queue = Queue(maxsize=10)
+        self.last_value = AhrsData()
         self._set_Process()
-        self.begin()
-    def begin(self):
+        self._begin()
+
+    def _begin(self):
         self.process.start()
         
     def _set_Process(self):
-        self.process = Process(target=_Ahrs_Process, args=(self.measures_queue,self.ahrs_serial,))
+        self.process = Process(target=_ahrs_Process, args=(self.ahrs_serial, self.measures_queue,))
 
     def end(self):
         self._set_Process()
         
-        
-    def getLastValue(self):
+    def get_last_value(self):
         if self.measures_queue.qsize() > 0:
             self.last_value = self.measures_queue.get()
         return self.last_value
-def _Ahrs_Process(serial:Serial ,queue:Queue):
+    
+
+def _ahrs_Process(serial: Serial, queue: Queue):
         while True:
-            try:
-                line = str(serial.readline(), encoding="ASCII")
-                if "AHRS" in line:
-                    data = line.split(';')
+            line = (str(serial.readline(), encoding="ASCII")).strip()
+            if "AHRS" in line:
+                data = line.split(';')
+                if queue.qsize() > 5:
+                   queue.get()
+                try:
                     queue.put(ahrs_data_to_point(data))
-            except:
-                print("readErr")        
+                except AhrsReadError:
+                    pass
+      
 def ahrs_data_to_point(data):
+    if len(data) < 9:
+        raise AhrsReadError
     a = InercialPoint()
     g = InercialPoint()
     m = InercialPoint()
-    try:
-        m.x = float(data[1])
-        m.y = float(data[2])
-        m.z = float(data[3])
-        g.x = float(data[4])
-        g.y = float(data[5])
-        g.z = float(data[6])
-        a.x = float(data[7])
-        a.y = float(data[8])
-        a.z = float(data[9])
-    except:
-        pass    
-    return AhrsData(a, g, m)       
-
-
+    m.x = float(data[1])
+    m.y = float(data[2])
+    m.z = float(data[3])
+    g.x = float(data[4])
+    g.y = float(data[5])
+    g.z = float(data[6])
+    a.x = float(data[7])
+    a.y = float(data[8])
+    a.z = float(data[9])
+    return AhrsData(a, g, m)   
