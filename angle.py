09cf8936a5615b59eb3f0ee93293ba5ea44dc687
Bartosz Orlof <bartorl153@student.polsl.pl>
2023-04-13 12:41:40 +0200

    mesuring errors fix

--- angle.py
+++ angle.py
@@ -1,6 +1,6 @@
 import math
 
-MAX_UWB_OFFSET_FACTOR: float = 1.15
+MAX_UWB_OFFSET_FACTOR: float = 1.1
 
 def angle_calculation(distance_l: float, distance_r: float, cross_used_distance:float=0.95):
     """
@@ -8,19 +8,24 @@ def angle_calculation(distance_l: float, distance_r: float, cross_used_distance:
     distance_r -distance to right module
     cross_used_distance - distance on the cross
     """
-    scale_offset_factor = 1.005
+    given_l = distance_l
+    given_r = distance_r
+    scale_offset_factor = 1.0025
 
 
     if(distance_l + cross_used_distance <= distance_r or distance_r + cross_used_distance <= distance_l):
-        #'''
+
         while(scale_offset_factor < MAX_UWB_OFFSET_FACTOR and (distance_l + cross_used_distance <= distance_r 
                                                                or distance_r + cross_used_distance <= distance_l)):
+            distance_l = given_l
+            distance_r = given_r
             if(distance_l + cross_used_distance <= distance_r):
                 distance_l*=scale_offset_factor
+                distance_r = distance_r * (2-scale_offset_factor)
             if(distance_r + cross_used_distance <= distance_l):
                 distance_r*=scale_offset_factor
-            scale_offset_factor += 0.005
-        #'''
+                distance_l = distance_l * (2-scale_offset_factor)
+            scale_offset_factor += 0.0025
    
     if(distance_l + cross_used_distance <= distance_r or distance_r + cross_used_distance <= distance_l):
         return 420.0
@@ -28,7 +33,6 @@ def angle_calculation(distance_l: float, distance_r: float, cross_used_distance:
     rads = math.acos(cos_a) #wynik w radianach
     degrees = math.degrees(rads) #wynik w stopniach
     return (rads, degrees)
-
 
 
 
