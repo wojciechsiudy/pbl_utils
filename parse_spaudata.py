431c8f13afd51d0aab9bec0e750d46298dd4d228
Wojciech Siudy <wojcsiu541@student.polsl.pl>
2023-05-22 14:48:18 +0200

    parse_spaudata: Make imports unrelative
    Signed-off-by: Wojciech Siudy <wojcsiu541@student.polsl.pl>

--- parse_spaudata.py
+++ parse_spaudata.py
@@ -1,7 +1,8 @@
-from .spausyncing import SpauData
-from .mapping import GpsData, Point
-from .ranging import UwbDataPair,UwbData
-from .inercing import AhrsData,InercialPoint
+from pbl_utils.mapping import GpsData, Point
+from pbl_utils.ranging import UwbDataPair,UwbData
+from pbl_utils.inercing import AhrsData,InercialPoint
+from pbl_utils.spausyncing import SpauData
+
 def process_file(filename:str):
     read_data = []
     with open(filename) as f:
@@ -12,10 +13,10 @@ def process_file(filename:str):
         if data[i+1].strip() != "True":
             i += 15
             continue
-        
+
         # Time
         time = float(data[i+3].strip())
-        
+
         # First UWB
         uwb_first_tag_address = data[i+5].strip().split(" ")[1]
         uwb_first_distance = float(data[i+5].strip().split(" ")[2])
@@ -28,20 +29,20 @@ def process_file(filename:str):
         uwb_second_power = float(data[i+6].strip().split(" ")[3])
         uwb_second_is_valid = data[i+6].strip().split(" ")[4] == "valid"
         second_UwbData = UwbData(uwb_second_tag_address, uwb_second_distance, uwb_second_power, uwb_second_is_valid)
-        
+
         uwb_Data = UwbDataPair(first_UwbData, second_UwbData)
-        
+
         # AHRS
         values = data[i+9].strip().split(" ")
         accel = InercialPoint(float(values[0]), float(values[1]), float(values[2]))
         gyro = InercialPoint(float(values[3]), float(values[4]), float(values[5]))
         mag = InercialPoint(float(values[6]), float(values[7]), float(values[8]))
         ahrs_Data = AhrsData(accel, gyro, mag)
-        
+
         # GPS
         values = data[i+11].strip().split(" ")
         gps_Data = GpsData(float(values[0]), float(values[1]))
-        
+
         # Calculated position
         values = data[i+13].strip().split(" ")
         x = float(values[1])
